7fae7d69c5d4e78efa96c495721a19ceab2e9a5d
Lindsey Baker <lindsey.baker0@gmail.com>
2019-04-05 14:05:34 -0700

    Add spaces to pushed string to seperate returned array elements

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,19 @@ function robot(userInput) {
 
 //Condition for 3's
     if (String(i).includes("3")){
-    rangeArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    rangeArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
 
 //Condition for 2's
     } else if (String(i).includes("2")){
-      rangeArray.push("BOOP");
+      rangeArray.push(" BOOP");
 
 //Condition for 2's
     } else if (String(i).includes("1")){
-      rangeArray.push("BEEP");
+      rangeArray.push(" BEEP");
 
 //Condition for numbers w/o 1,2 or 3
     } else {
-      rangeArray.push(i);
+      rangeArray.push(" " + i);
     }
   }
   return rangeArray
