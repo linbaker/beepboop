b285f07cef0c3c1e14551591cfc30b04f378b63d
Lindsey Baker <lindsey.baker0@gmail.com>
2019-04-05 17:09:05 -0700

    Stop return at number UserInput 50 unless user clicks second button

--- js/scripts.js
+++ js/scripts.js
@@ -16,31 +16,77 @@ function robot(userInput, userName) {
     userName = "Dave";
   }
 
-  for (var i = 0; i <= numberUserInput; i++) {
+if (numberUserInput > 50) {
+  $(overFiftyOutput).show();
+  for (var i = 0; i <= 50; i++) {
+  //Condition for 3's
+      if (String(i).includes("3")){
+      rangeArray.push(" I'm sorry, " + userName + ". I'm afraid I can't do that.");
 
-//Condition for 3's
-    if (String(i).includes("3")){
-    rangeArray.push(" I'm sorry, " + userName + ". I'm afraid I can't do that.");
+  //Condition for 2's
+      } else if (String(i).includes("2")){
+        rangeArray.push(" BOOP");
 
-//Condition for 2's
-    } else if (String(i).includes("2")){
-      rangeArray.push(" BOOP");
+  //Condition for 2's
+      } else if (String(i).includes("1")){
+        rangeArray.push(" BEEP");
 
-//Condition for 2's
-    } else if (String(i).includes("1")){
-      rangeArray.push(" BEEP");
-
-//Condition for numbers w/o 1,2 or 3
-    } else {
-      rangeArray.push(" " + i);
+  //Condition for numbers w/o 1,2 or 3
+      } else {
+        rangeArray.push(" " + i);
+      }
     }
-  }
+  } else if (numberUserInput < 50) {
+    $(overFiftyOutput).hide;
+    for (var i = 0; i <= numberUserInput; i++) {
 
-  if (rangeArray.length === 0) {
-    return
+    //Condition for 3's
+        if (String(i).includes("3")){
+        rangeArray.push(" I'm sorry, " + userName + ". I'm afraid I can't do that.");
+
+    //Condition for 2's
+        } else if (String(i).includes("2")){
+          rangeArray.push(" BOOP");
+
+    //Condition for 2's
+        } else if (String(i).includes("1")){
+          rangeArray.push(" BEEP");
+
+    //Condition for numbers w/o 1,2 or 3
+        } else {
+          rangeArray.push(" " + i);
+        }
+      }
   }
   return rangeArray
 }
+
+function robotExcessive(userInput, userName) {
+  var numberUserInput = parseInt(userInput);
+
+  var rangeArray = [];
+
+  for (var i = 50; i <= numberUserInput; i++) {
+
+  //Condition for 3's
+      if (String(i).includes("3")){
+      rangeArray.push(" I'm sorry, " + userName + ". I'm afraid I can't do that.");
+
+  //Condition for 2's
+      } else if (String(i).includes("2")){
+        rangeArray.push(" BOOP");
+
+  //Condition for 2's
+      } else if (String(i).includes("1")){
+        rangeArray.push(" BEEP");
+
+  //Condition for numbers w/o 1,2 or 3
+      } else {
+        rangeArray.push(" " + i);
+      }
+    }
+    return rangeArray
+  }
 
 
 //FRONTEND
@@ -51,9 +97,22 @@ $(function(){
     var userName = $("#inputName").val();
 
     var output = robot(userInput, userName);
-    $("#answer").show();
+    $(".answer").show();
 
     $("#outputToUser").text(output);
+    event.preventDefault();
+  });
+  $("#secondButton").submit(function(event) {
+    var userInput = $("#inputNumber").val();
+
+    var userName = $("#inputName").val();
+
+    var outputExcessive = robotExcessive(userInput, userName);
+
+    $(".answer").show();
+    $("#overFiftyOutput").hide();
+
+    $("#outputToUser").append(outputExcessive);
     event.preventDefault();
   });
 });
